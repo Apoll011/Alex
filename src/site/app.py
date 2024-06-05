b835d747fdedde555077afb515c469e64c1fae3f
Apoll011 <tiagorobotik@gmail.com>
2024-06-05 11:27:41 -0100

    added notifications

--- src/site/app.py
+++ src/site/app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from datetime import datetime
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'eryh840tiwry48ouehrjg8yoeuhgye58ohrugd'
 @app.route('/') 
 def index():
-    return render_template('index.html', user_name="Tiago Bernardo", date="4 tue Sat", climate = {"icon": "sun", "degree": "24", "city": "Mindelo", "country": "Cape Vert"})
+    flash("Hithere")
+    return render_template('index.html',len=len, user = {"name": "Tiago Bernardo"}, 
+                           climate = {"icon": "sun", "degree": "24", "city": "Mindelo", "country": "Cape Vert", "date": datetime.now()},
+                           notifications = [
+                                {
+                                    "title": "Aplication Error Fixed",
+                                    "date": "26 JUL 23 (10:26)",
+                                    "description": "The Error ID#4523624 of Type 2 has been fixed",
+                                    "icon": "info-alt",
+                                    "bg": "success"
+                                },
+                                {
+                                    "title": "New user registration",
+                                    "date": "26 JUL 23 (10:28)",
+                                    "description": "User 524 was been learn",
+                                    "icon": "user",
+                                    "bg": "info"
+                                },
+                                {
+                                    "title": "System Started",
+                                    "date": "26 JUL 23 (10:30)",
+                                    "description": "Alex System Have Started",
+                                    "icon": "settings",
+                                    "bg": "warning",
+                                    "costume_class": "infinite-spin"
+                                }
+                            ]
+                           )
 
 @app.route('/plugins.html') 
 def plugins():
