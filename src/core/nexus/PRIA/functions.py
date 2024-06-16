a7b55e237b9b749c66a5913bab46c79e5b665588
Apoll011 <tiagorobotik@gmail.com>
2024-06-16 15:37:49 -0100

    Added key to alex context in pria init functions

--- src/core/nexus/PRIA/functions.py
+++ src/core/nexus/PRIA/functions.py
@@ -1,10 +1,16 @@
-from ..ai import AiInitActionBlueprint
+from ..ai import AiBluePrintSkeleton, AI
 from ...system.api.call import ApiCall
+from core.system.security._key import AlexKey
 
-priaInitAction = AiInitActionBlueprint()
+priaSkeleton = AiBluePrintSkeleton()
 api = ApiCall("127.0.0.1", 1178)
 
-@priaInitAction.init_action("Get Master User")
-def get_master_user(pria):
+@priaSkeleton.init_action("Get Master User")
+def get_master_user(self, pria):
     p = api.call_route_async("users/search/tags", {"query": "Master"})
-    p.then(lambda user: pria.finish_and_set("Get Master User", "master", user.responce["users"][0]))
+    p.then(lambda user: pria.finish_and_set(self, "master", user.responce["users"][0]))
+
+@priaSkeleton.init_action("Set context")
+def set_context(self, pria: AI):
+    pria.set_context("key", AlexKey.get())
+    pria.finish(self)
