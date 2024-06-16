0255d662f7667f47fb61c4525edc8515571dcec8
Apoll011 <tiagorobotik@gmail.com>
2024-06-16 17:47:59 -0100

    Changed TRanslation System

--- src/core/system/translate.py
+++ src/core/system/translate.py
@@ -1,50 +1,72 @@
-from .config import __lang__,path
-from .error import Error
+# translation_system.py
+from .config import path
 
-#TODO: Change the Translation mechanismes
-#Start Translate
-__LangExt__ = ".lang"
-class GTranslateModule:
-    translationIndex = {}
-    def __init__ (self, langg = __lang__, file = "system"):
-        basePath = path + f"/language/{langg}/tradutor/{file}" + __LangExt__
-        f = open(basePath, "r", encoding = "UTF-8").read().splitlines()
-        for l in f:
-            if l != "":
-                tran = l.split(":")
-                self.translationIndex[tran[0]] = tran[1].replace("#%", "{}")
-    def getTranslation(self, lookup):
-        cv = self.translationIndex[lookup]
-        return cv
+class TranslationSystem:
+    """
+    A translation system that loads translations from files and provides a way to retrieve them.
 
-gt = GTranslateModule()
-
-def __(Jumpper, *args):
-    """Jumpper = ex: system.tanks
-    predef lang = pt
-    return Obrigado
+    Attributes:
+        lang (str): The language code (e.g. "en", "pt", etc.)
+        file (str): The file name (e.g. "system", "game", etc.)
+        translations (dict): A dictionary of translations, where keys are translation keys and values are translated strings
     """
 
-    try:
-        if(len(args) > 0):
-                        return str(gt.getTranslation(Jumpper)).format(*args)
-        else:
-            return str(gt.getTranslation(Jumpper))
-    except KeyError:
-                return __("error."+str(Error.get(457)[1]))
-    except Exception as e:
-        return __("error."+str(Error.get(e)[1]))
+    def __init__(self, lang: str, file: str = "system") -> None:
+        """
+        Initializes the translation system.
 
-class Translator:
-    def __init__(self, file= "system", lang = __lang__) -> None:
-        self.gt = GTranslateModule(file = file, langg = lang)
-    def _(self, Jumpper, *args):
+        Args:
+            lang (str): The language code (e.g. "en", "pt", etc.)
+            file (str): The file name (e.g. "system", "game", etc.)
+        """
+        self.lang = lang
+        self.file = file
+        self.translations = self.load_translations()
+
+    def load_translations(self) -> dict:
+        """
+        Loads translations from a file.
+
+        Returns:
+            dict: A dictionary of translations, where keys are translation keys and values are translated strings
+        """
+        file_path = f"{path}/resources/language/{self.lang}/tradutor/{self.file}.lang"
+        with open(file_path, "r", encoding="UTF-8") as f:
+            translations = {}
+            for line in f:
+                if line.strip():
+                    key, value = line.strip().split(":", 1)
+                    translations[key] = value.strip().replace("#%", "{}")
+            return translations
+
+    def get_translation(self, key: str, *args) -> str:
+        """
+        Retrieves a translation for a given key.
+
+        Args:
+            key (str): The translation key
+            *args: Optional arguments to format the translation string
+
+        Returns:
+            str: The translated string
+        """
         try:
-            if(len(args) > 0):
-                            return str(self.gt.getTranslation(Jumpper)).format(*args)
-            else:
-                return str(self.gt.getTranslation(Jumpper))
+            translation = self.translations[key]
+            if args:
+                return translation.format(*args)
+            return translation
         except KeyError:
-                    return __("error."+str(Error.get(457)[1]))
-        except Exception as e:
-            return __("error."+str(Error.get(e)[1]))
+            return self.get_translation("error.457", key)
+
+    def __call__(self, key: str, *args) -> str:
+        """
+        Convenience method to retrieve a translation.
+
+        Args:
+            key (str): The translation key
+            *args: Optional arguments to format the translation string
+
+        Returns:
+            str: The translated string
+        """
+        return self.get_translation(key, *args)
