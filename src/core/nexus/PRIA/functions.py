d83e096e243969408f9e8e6b0f268b34063fb470
Apoll011 <tiagorobotik@gmail.com>
2024-06-17 12:11:01 -0100

    Added init action for api in pria

--- src/core/nexus/PRIA/functions.py
+++ src/core/nexus/PRIA/functions.py
@@ -3,12 +3,16 @@ from core.system.api.call import ApiCall
 from core.system.security._key import AlexKey
 
 priaSkeleton = AiBluePrintSkeleton()
-api = ApiCall("127.0.0.1", 1178)
+
+@priaSkeleton.init_action("Set Api conection")
+def set_api_con(self, pria: AI):
+    pria.api = ApiCall("127.0.0.1", 1178)
+    pria.finish(self)
 
 @priaSkeleton.init_action("Get Master User")
 def get_master_user(self, pria: AI):
-    p = api.call_route_async("users/search/tags", {"query": "Master"})
-    p.then(lambda user: pria.finish_and_set(self, "master", api.call_route("users/get", user.responce["users"][0]).responce))
+    p = pria.api.call_route_async("users/search/tags", {"query": "Master"})
+    p.then(lambda user: pria.finish_and_set(self, "master", pria.api.call_route("users/get", user.responce["users"][0]).responce))
 
 @priaSkeleton.init_action("Set context")
 def set_context(self, pria: AI):
