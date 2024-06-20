845482d8940209e1b6dbb7bd7b6660caa928b3e3
Apoll011 <tiagorobotik@gmail.com>
2024-06-20 16:34:54 -0100

    Added features to test skill

--- src/skills/testing/__main__.py
+++ src/skills/testing/__main__.py
@@ -14,23 +14,27 @@ class EvenOrOdd(BaseSkill):
 
      def is_prime(self):
           if self.slots["number"].value > self.prime_search_limit:
-                    return self.responce("Sorry. This number is bigger than my upper search limit.")
-          for i in range(2, self.slots["number"].value/2):
+               return self.responce("Sorry. This number is bigger than my upper search limit.")
+          
+          for i in range(2, int(self.slots["number"].value//2)):
                if self.slots["number"].value % i == 0:
-                    return self.responce(f"Yes {self.slots["number"].value} is prime")
+                    return self.responce(f"No {self.slots["number"].value} is not prime")
+          
+          return self.responce(f"Yes {self.slots["number"].value} is prime")
                
      def execute(self, alex, intent):
           super().execute(alex, intent)
           self.require("number", SlotValueNumber)
           self.optional("type", SlotValue)
 
-          if self.slots["type"] and self.slots["type"] == "prime":
+          if self.slots["type"] and self.slots["type"].value == "prime":
                return self.is_prime()
-          elif self.slots["type"] and self.slots["type"] == "even":
+          elif self.slots["type"] and self.slots["type"].value == "even":
                self.make_responce("Yes", "No")
-          elif self.slots["type"] and self.slots["type"] == "odd":
+          elif self.slots["type"] and self.slots["type"].value == "odd":
                self.make_responce("No", "Yes")
           else:
+               print(self.slots["type"], self.slots["type"])
                self.make_responce()
      
      def make_responce(self, first = "", second = ""):
