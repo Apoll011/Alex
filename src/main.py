50c698fbd0b7332e1ba1d7278342e3136884f73f
Apoll011 <tiagorobotik@gmail.com>
2024-06-20 18:57:42 -0100

    Added argarse

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 from core.nexus.ai import Nexus
+from core.system.config import __version__
+import argparse
 
-Nexus.start_nexus()
 
-#Nexus.request_ai("ALEX", "retrain")
+class InstallSkill(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        file = values
 
-Nexus.call_ai("PRIA", "start")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--install-skill", action=InstallSkill, help="Install a skill")
+parser.add_argument("-t", "--train", action="store_true", help="Train all the resources from Alex and exit")
+parser.add_argument("-s", "--start", action="store_true", help="Start Alex")
+
+parser.add_argument("-v", "--version", action="version", version=f"Alex {__version__}")
+
+args = parser.parse_args()
+print(args)
+if args.train or args.start:
+    Nexus.start_nexus()
+    if args.train:
+        Nexus.request_ai("ALEX", "retrain")
+    else:
+        Nexus.call_ai("PRIA", "start")
