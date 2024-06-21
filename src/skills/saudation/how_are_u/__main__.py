f9124ce39fb1ba3164aee668dd998400f571d299
Apoll011 <tiagorobotik@gmail.com>
2024-06-21 10:19:46 -0100

    Added messag of in constroction for howareu skill

--- src/skills/saudation/how_are_u/__main__.py
+++ src/skills/saudation/how_are_u/__main__.py
@@ -1,25 +1,12 @@
 from core.system.skills.call import SkillCaller
 from core.system.skills import BaseSkill
 
-class AreUSure(BaseSkill):
+class HowAreU(BaseSkill):
      def __init__(self):
-          self.register("saudation@are.u.sure")
+          self.register("saudation@how.are.you")
           super().__init__()
 
      def execute(self, context, intent):
           super().execute(context, intent)
 
-          last_responce = self.alex_context.load("last_responce")
-          last_intent = self.alex_context.load("last_intent")
-          
-          skill = SkillCaller().call(last_intent) # type: ignore
-          skill.set_as_api()
-          new_value = skill.execute(context, last_intent) # type: ignore
-
-          if new_value  == last_responce: 
-               self.responce(self.translate.get_translation("confirmation.yes"))
-          else:
-               self.responce(self.translate.get_translation("confirmation.no", new_value))
-
-     def responce(self, text):
-          self.speak(text)
+          self.responce("The AI HIANE is not set yet. We from Nexus apreciate your pacience while your creator is working on it.")   
