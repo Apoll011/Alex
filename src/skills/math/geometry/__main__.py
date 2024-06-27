06feb75ae757686a27950cc550a30e54c65fcf9b
Apoll011 <tiagorobotik@gmail.com>
2024-06-27 14:34:55 -0100

    Added geometry support with geometry skill

---
+++ src/skills/math/geometry/__main__.py
@@ -0,0 +1,27 @@
+from core.system.intents.slots import SlotValueNumber, SlotValue
+from core.system.skills import BaseSkill
+from math import sin, cos, sqrt, cosh, sinh, tan, tanh
+
+class Geometry(BaseSkill):
+     def __init__(self):
+          self.register("math@geometry")
+          super().__init__()
+
+     def execute(self, context, intent):
+          super().execute(context, intent)
+          self.require("func", SlotValue)
+          self.require("number", SlotValueNumber)
+
+          r = None 
+
+          if self.slots['func'].value == "sen":
+               r = sin(self.slots["number"].value)
+
+          elif self.slots['func'].value == "cos":
+               r = cos(self.slots["number"].value)
+          
+          self.alex_context.save(r, "last_result")
+          
+          return self.responce_translated("result", r)
+
+     
