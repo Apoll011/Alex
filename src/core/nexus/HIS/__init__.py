0add47931da52cd011d7aaeb4c2fc2ff059d797a
Apoll011 <tiagorobotik@gmail.com>
2024-07-03 13:53:35 -0100

    removed test variables from ai HIS

--- src/core/nexus/HIS/__init__.py
+++ src/core/nexus/HIS/__init__.py
@@ -1,12 +1,5 @@
 from core.system.ai.ai import AI
 
 class HIS(AI):
-    h = False
-    f = False
     def __init__(self) -> None:
         super().__init__("HIS")
-        self.h = True
-    
-    def activate(self):
-        self.f = True
-        super().activate()
