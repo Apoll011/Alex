9a251d78b3a2e365909d6ab273fd70fcb8e09bad
Apoll011 <tiagorobotik@gmail.com>
2024-07-04 17:54:52 -0100

    Added the base functions to wec

--- src/core/nexus/WEC/__init__.py
+++ src/core/nexus/WEC/__init__.py
@@ -1,5 +1,12 @@
+from .functions import wecSkeleton
 from core.system.ai.ai import AI
 
+
 class WEC(AI):
+    database = {
+        
+    }
     def __init__(self) -> None:
         super().__init__("WEC")
+
+        self.register_blueprint(wecSkeleton)
