e6bf288f46e398467409b47b03dfc1361f9ccbf0
Apoll011 <tiagorobotik@gmail.com>
2024-07-04 17:07:17 -0100

    Added two new AIs

---
+++ src/core/nexus/MIM/__init__.py
@@ -0,0 +1,5 @@
+from core.system.ai.ai import AI
+
+class MIM(AI):
+    def __init__(self) -> None:
+        super().__init__("MIM")
---
+++ src/core/nexus/RIG/__init__.py
@@ -0,0 +1,5 @@
+from core.system.ai.ai import AI
+
+class RIG(AI):
+    def __init__(self) -> None:
+        super().__init__("RIG")
