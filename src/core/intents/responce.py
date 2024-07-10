8860c7be50f57e1306f88530aaeb6331121d5bb4
Apoll011 <tiagorobotik@gmail.com>
2024-07-10 17:04:08 -0100

    Added two new responce types

--- src/core/intents/responce.py
+++ src/core/intents/responce.py
@@ -31,10 +31,37 @@ class Responce:
                return text
 
 class AnyResponce(Responce):
-     rtype = lambda x: x
-     
      def is_accepted(self, text) -> bool:
           self.result = text
+          return True
+
+class SomethingFromListOrNoneResponce(Responce):
+     replace = {
+          "none": None,
+     }
+
+     def __init__(self, list_word) -> None:
+          for e in list_word:
+               self.replace.update({e: 1})
+          self.list = list_word
+
+     def is_accepted(self, text) -> bool:
+          if "none" in text:
+               self.result = None
+               return True
+          
+          for elem in self.list:
+               if text.lower() in elem.lower():
+                    self.result = text
+                    return True
+          
+          return False
+
+class SomethingOrNoneResponce(Responce):
+     def is_accepted(self, text) -> bool:
+          self.result = text
+          if "none" in text:
+               self.result = None
           return True
 
 class BoolResponce(Responce):
