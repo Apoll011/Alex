f3c132b38ee50e097b90a1e23919e34ed55f8c90
Apoll011 <tiagorobotik@gmail.com>
2024-07-11 18:39:47 -0100

    fixed a bug in the function get_time_of_day. Of comparing a grater value in the same time as a smaller value. And by fixing it antoher bu appear an I fix it too

--- src/core/date.py
+++ src/core/date.py
@@ -2,15 +2,14 @@ import time
 import datetime
 
 
-def get_time_of_day() -> int:
+def get_time_of_day():
     """
     Returns either 1, 2 or 3 for the different times of a day: morning, affternon, night. Respectivlly.
     """
     h = datetime.datetime.now().hour
-    if h >= 7 and h < 12:
-        return 1
-    elif h >= 12 and h < 18:
-        return 2
-    elif h >= 18 and h < 7:
+    if h > 18 or h < 7:
         return 3
-    return 0
+    elif h >= 12:
+        return 2
+    elif h >= 7:
+        return 1
