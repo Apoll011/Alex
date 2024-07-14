f0556ca6e606a34935d78fc5b8e706a13fd03034
Apoll011 <tiagorobotik@gmail.com>
2024-07-14 17:33:47 -0100

    Improved argparse experience by adding an language flag

--- src/main.py
+++ src/main.py
@@ -11,10 +11,10 @@ class InstallSkill(argparse.Action):
         intent = values[1] # type: ignore
         print(f"\33[32mStarting Instalation of \33[33m{intent}\33[0m")
         s = intent.split("@")
-        path = f"{s[0]}/{s[1].replace(".", "_")}/"
+        path = f"{s[0]}/{s[1].replace('.', '_')}/"
         with zipfile.ZipFile(file, 'r') as zip_ref: # type: ignore
             zip_ref.extractall('./src/skills/'+path)
-            os.system(f"rm -rf ./src/skills/{path}/__MACOSX/")
+            os.system(f"rm -rf./src/skills/{path}/__MACOSX/")
             zip_ref.close()
         print(f"\33[32mEnded Instalation of \33[33m{intent}\33[0m")
 
@@ -23,23 +23,24 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("-p", "--install-skill", action=InstallSkill, nargs=2, help="Install a skill")
 parser.add_argument("-t", "--train", action="store_true", help="Train all the resources from Alex and exit")
-parser.add_argument("-s", "--start", help="Start Alex", choices=["en", "pt"])
+parser.add_argument("-l", "--language", default="en", help="Set the language", choices=["en", "pt"])
+parser.add_argument("-s", "--start", action="store_true", help="Start Alex")
 parser.add_argument("-d", "--debug", action="store_true", help="Enters Debug Mode")
-parser.add_argument("-i", "--interface", help="Interface mode" , default="cmd", choices=["cmd", "server", "voice"])
+parser.add_argument("-i", "--interface", default="cmd", help="Interface mode", choices=["cmd", "server", "voice"])
 
-parser.add_argument("-v", "--version", action="version", version=f"Alex {VersionManager.get().get("coreVersion", "")}")
+parser.add_argument("-v", "--version", action="version", version=f"Alex {VersionManager.get().get('coreVersion', '')}")
 
 args = parser.parse_args()
 
 def main(args):
-    language = args.start
+    language = args.language
 
     alex = ALEX()
     
     if args.train:
         alex.handle_request("retrain")
 
-    if not language == None:
+    if args.start_alex:
         alex.set_language(language)
         alex.activate()
 
