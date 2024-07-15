8b5dec159036d1e15e2c6a1c5331379f4683e8b8
Apoll011 <tiagorobotik@gmail.com>
2024-07-15 17:00:18 -0100

    Now if the In cmd is expecting a message in this case INput is on do some thing to prevent text overlapp

--- src/core/interface/cmd.py
+++ src/core/interface/cmd.py
@@ -4,19 +4,25 @@ from core.interface.base import BaseInterface
 
 
 class ComandLine(BaseInterface):
+    
+    waiting_for_message = False
+
     def start(self):
         self.user_conect({})
         super().start()
 
     def speak(self, data: dict[str, str | IntentResponse], voice: str = 'Alex', voice_command = None, voice_mode = False):
         if data['message'] != "":
-            print(f"{voice}: \33[36m{data['message']}\33[0m")
-
+            if self.waiting_for_message:
+                print()
+            print(f"\33[0m{voice}: \33[36m{data['message']}\33[0m")
             if voice_mode:
                 Voice.s(data, voice, voice_command, False)
     
     def loop(self):
+        self.waiting_for_message = True
         message = input(f"{self.request_sentence}: \33[32m")
         print("\33[0m", end="")
-        super().loop()
+        self.waiting_for_message = False
         self.input({"message": message})
+        super().loop()
