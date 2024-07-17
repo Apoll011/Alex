8159011fb6fa5c36d1812b8135973e3473a34a9a
Apoll011 <tiagorobotik@gmail.com>
2024-07-17 15:21:40 -0100

    Added the skill set reminder

--- src/skills/set/reminder/__main__.py
+++ src/skills/set/reminder/__main__.py
@@ -1,6 +1,6 @@
 import json
 import uuid
-from dataclasses import dataclass
+from core.log import LOG
 from core.skills import BaseSkill
 from core.intents.slots import *
 from core.config import EventPriority
@@ -13,12 +13,12 @@ class ReminderObject:
      action: SlotValue
      person: SlotValue | None
 
-     def __init__(self, time: SlotValueInstantTime, action: SlotValue, person: SlotValue | None = None) -> None:
+     def __init__(self, time: SlotValueInstantTime, action: SlotValue, person: SlotValue | None = None, uid: str | None = None) -> None:
           self.time = time
           self.action = action
           self.person = person
 
-          self.id = str(uuid.uuid4())
+          self.id = str(uuid.uuid4()) if uid == None else uid
 
      def get_hour_and_minute(self):
           if self.time.get_hour() == "00" and self.time.get_minute() == "00":
@@ -49,15 +49,31 @@ class ReminderObject:
           with open(DataFile.load(self.id, "reminder"), "w") as remind:
                json.dump(Reminderjson, remind)
 
-     def load(self, uuid):
+     @staticmethod
+     def load(uuid):
           d = DataFile.get(uuid, "reminder")
           reminder_json = json.loads(d)
-          self.id = reminder_json["id"]
-          self.time = SlotValueInstantTime(reminder_json["time"]["kind"], reminder_json["time"]["value"], reminder_json["time"]["grain"], reminder_json["time"]["precision"])
-          self.action = SlotValue(reminder_json["action"]["kind"], reminder_json["action"]["value"])
-          self.person = None
+          id = reminder_json["id"]
+          time = SlotValueInstantTime(reminder_json["time"]["kind"], reminder_json["time"]["value"], reminder_json["time"]["grain"], reminder_json["time"]["precision"])
+          action = SlotValue(reminder_json["action"]["kind"], reminder_json["action"]["value"])
+          person = None
           if reminder_json["person"] != None:
-               self.person = SlotValue(reminder_json["person"]["kind"], reminder_json["person"]["value"])
+               person = SlotValue(reminder_json["person"]["kind"], reminder_json["person"]["value"])
+          return ReminderObject(time, action, person, id)
+     
+     def seconds(self):
+          return self.time.diference_from_now_to_value().total_seconds()
+
+     def get_action(self):
+          return self.action.value    
+
+     def schedule(self, alex, callback):
+          seconds = self.seconds()
+          if seconds > 0:
+               alex.schedule(seconds, EventPriority.SKILLS, callback, self.id)
+          else:
+               LOG.debug("removed reminder of id: ", self.id)
+               DataFile.delete(self.id, "reminder")
 
 class Reminder(BaseSkill):
      def init(self):
@@ -66,7 +82,6 @@ class Reminder(BaseSkill):
           
      def execute(self, context, intent):
           super().execute(context, intent)
-          print(intent.json)
           self.require("time", SlotValueInstantTime)
           self.require("action")
           self.optional("person")
@@ -76,5 +91,12 @@ class Reminder(BaseSkill):
                reminder = ReminderObject(self.slots["time"], self.slots["action"], self.slots["person"]) # type: ignore
 
           reminder.save()
-          print(reminder.time.to_datetime())
           
+          reminder.schedule(self.alex(), self.fire_reminder)
+          
+          self.responce_translated("reminder.set", {"time": self.get_raw_slot_value("time"), "action": reminder.get_action()})
+
+     def fire_reminder(self, id):
+          reminder = ReminderObject.load(id)
+
+          self.responce_translated("reminder.fire", {"action": reminder.get_action()})
