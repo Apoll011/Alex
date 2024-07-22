acf7b36745380dbc35c2d4218dc2b02a6aa92b01
Apoll011 <tiagorobotik@gmail.com>
2024-07-22 15:16:30 -0100

    improved voice interface

--- src/core/interface/voice.py
+++ src/core/interface/voice.py
@@ -34,8 +34,6 @@ class Voice(BaseInterface):
         return Voice.alex_possibilities[lang][preference]
 
     def loop(self):
-        self.alex.clear()
-        self.print_header()
         message = input(f"")
         if message == "":
             self.wakeword({})
@@ -48,13 +46,8 @@ class Voice(BaseInterface):
 
     def on_wake_word(self):
         self.waiting_for_message = True
-        try:
-            message = self.listen()
-            print(len(message))
-            print(message, "f")
-        except NotImplementedError:
-            message = input(f"{self.request_sentence}: \33[32m")
-            print("\33[0m")
+        message = self.listen()
+        print(f"{self.request_sentence}: \33[32m {message} \33[0m")
         self.waiting_for_message = False
         self.input({"message": message})   
 
@@ -80,10 +73,6 @@ class Voice(BaseInterface):
         os.system(command)
 
     def listen(self):
-        if self.alex.debug_mode:
-            print("Listening...")
-            c = "hear -m -p -t 2"
-            result = subprocess.check_output(c, shell=True, text=True)
-            return result.strip().replace("\n", "").strip().lstrip().rstrip()
-        else:
-            raise NotImplementedError("Listen Function is not Implemented yet for Voice Interface")
+        c = "hear -p -t 2"
+        result = subprocess.check_output(c, shell=True, text=True)
+        return result.strip().split("\n")[-1].strip().lstrip().rstrip()
