8e1385a1d27607cd56365590d2df2594b6f15d38
Apoll011 <tiagorobotik@gmail.com>
2024-07-25 13:23:32 -0100

    Added list clear skill

---
+++ src/skills/list/clear/__main__.py
@@ -0,0 +1,75 @@
+import os
+from core.skills import BaseSkill
+from core.resources.data_files import List
+from core.intents.responce import BoolResponce
+
+class Clear(BaseSkill):
+     def init(self):
+          self.register("list@clear")
+          self.can_go_again = False          
+
+     def execute(self, context, intent):
+          super().execute(context, intent)
+          self.optional("list")
+          self.optional("entity")
+
+          if not self.slot_exists("list"):
+               self.question("get.list", self.get_list)
+          else:
+               self.list = self.get("list").lower()
+               self.remove()
+
+     def get_list(self, responce):
+          self.list = responce.lower()
+          self.remove()
+
+     def remove(self):
+          if self.slot_exists("entity"):
+               self.entity = self.get("entity").lower()
+               question = "entity"
+               func = self.confirm_entity
+          else:
+               self.entity = ""
+               question = "list"
+               func = self.confirm_list
+          
+          if self.ensure_exists():
+               self.question(f"confirm.{question}", func, {"entity": self.entity, "list": self.list}, BoolResponce())
+
+
+     def ensure_exists(self) -> bool:
+          list_directory_map = os.listdir(List.getBasePath())
+          full_list_filename = f"{self.list}.list"
+          if full_list_filename in list_directory_map:
+               list_content = List.get(self.list)
+               if self.entity != "" and self.entity in list_content: # Here Will See If user provide entity and if the entity is in the requested list. Then delete entity.
+                    return True
+               elif self.entity == "": # Here if user didnt provid entity .And because list exists. clear list.
+                    return True
+               else: # Entity was provided but dont exist.
+                    self.responce_translated("did.not.found.entity", {"list": self.list, "entity": self.entity})
+          else:
+               self.responce_translated("did.not.found.list", {"list": self.list})
+          return False     
+
+     def confirm_list(self, responce): 
+          if responce:
+               self.delete_list()
+          else:
+               self.responce_translated("dont.delete")
+
+     def confirm_entity(self, responce):
+          if responce:
+               self.delete_entity()
+          else:
+               self.responce_translated("dont.delete")
+
+     def delete_list(self):
+          List.save(self.list, "", "w")
+          self.responce_translated("delete.list", {"list": self.list})          
+
+     def delete_entity(self):
+          list_content = List.get(self.list)
+          list_content.remove(self.entity)
+          List.save(self.list, "\n".join(list_content), "w")
+          self.responce_translated("delete.entity", {"list": self.list, "entity": self.entity})
