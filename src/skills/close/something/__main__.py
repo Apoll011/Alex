415da77bf0faaa0e48a6906cb120c4daab102823
Apoll011 <tiagorobotik@gmail.com>
2024-10-25 19:38:51 -0100

    Added skill for closing apps
    Signed-off-by: Apoll011 <tiagorobotik@gmail.com>

--- src/skills/close/something/__main__.py
+++ src/skills/close/something/__main__.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+
+from core.intents.responce import BoolResponce
 from core.skills import BaseSkill
 
 class Something(BaseSkill):
@@ -8,3 +12,18 @@ class Something(BaseSkill):
     def execute(self, intent):
         super().execute(intent)
         self.require("entityName")
+
+        c = f"pidof -d \";\" {self.get("entityName")}"
+        pids = subprocess.check_output(c, shell=True, text=True)
+        self.pids = pids.split(";")
+        self.question("confirmation.ask", self.confirmation, {"name": self.get("entityName")}, BoolResponce())
+
+    def confirmation(self, responce: bool):
+        if responce:
+            self.kill()
+        else:
+            self.say("cancel.kill")
+
+    def kill(self):
+        for pid in self.pids:
+            os.system(f"kill {pid}")
