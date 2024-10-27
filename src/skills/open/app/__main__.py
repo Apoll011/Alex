569c414fcce1ea9455f2d8bcf08024efe59acad6
Apoll011 <tiagorobotik@gmail.com>
2024-10-27 16:46:13 -0100

    Added ability to open folders
    Signed-off-by: Apoll011 <tiagorobotik@gmail.com>

--- src/skills/open/app/__main__.py
+++ src/skills/open/app/__main__.py
@@ -18,8 +18,10 @@ class App(BaseSkill):
             self.say("not.found", name=name.title())
 
     def open(self, name):
-        if name in self.skill_settings["links"]:
+        if name.lower() in self.skill_settings["links"]:
             c = f"nohup open {self.skill_settings["links"][name]} > /dev/null 2>&1 &"
+        elif name.lower() in self.skill_settings["folders"]:
+            c = f"nohup open {self.skill_settings["folders"][name]} > /dev/null 2>&1 &"
         else:
             subprocess.check_output(f"which {name}", shell=True, text=True)
             c = f"nohup {name} > /dev/null 2>&1 &"
